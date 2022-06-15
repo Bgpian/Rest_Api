4ce6c9751eeda2607f20d152830f67f913101ddf
Bgpian <piancungkring@gmail.com>
2022-06-15 19:35:41 +0700

    tes

---
+++ app/src/main/java/com/owenwikaliono/rest_api_181103448/Model/GetKontak.java
@@ -0,0 +1,36 @@
+package com.owenwikaliono.rest_api_181103448.Model;
+
+import com.google.gson.annotations.SerializedName;
+import java.util.List;
+
+public class GetKontak {
+    @SerializedName("status")
+    private String status;
+    @SerializedName("result")
+    List<Kontak> listdatakontak;
+    String message;
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public List<Kontak> getListdatakontak() {
+        return listdatakontak;
+    }
+
+    public void setListdatakontak(List<Kontak> listdatakontak) {
+        this.listdatakontak = listdatakontak;
+    }
+}
